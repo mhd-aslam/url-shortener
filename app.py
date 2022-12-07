a7b2cbc00926bdcca461c20f2b8c2cda19f0af87
Mohammed Aslam <aslampattam24@gmail.com>
2022-12-07 22:01:57 +0530

    Finish all basic features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 from werkzeug.utils import secure_filename
 import os.path
 import json
@@ -8,7 +8,7 @@ app.secret_key = '8h2tf629h20242gv2h2u2u'
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('home.html', codes=session.keys())
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -28,11 +28,12 @@ def your_url():
 		else:
 			f = request.files['file']
 			full_name = request.form['code'] + secure_filename(f.filename)
-			f.save('./images/' + full_name)
+			f.save('./static/user_files/' + full_name)
 			urls[request.form['code']] = {'file': full_name}
 
 		with open('./links/urls.json', 'w') as urls_file:
 			json.dump(urls, urls_file)
+			session[request.form['code']] = True
 
 		return render_template('your_url.html', code=request.form['code'])
 	else:
@@ -46,6 +47,17 @@ def redirect_to_url(code):
 			urls = json.load(urls_file)
 			if code in urls.keys():
 				if 'url' in urls[code].keys():
-					return redirect(urls[code]['url'])	
+					return redirect(urls[code]['url'])
+				else:
+					return redirect(url_for('static', filename='user_files/' + urls[code]['file']))	
+
+	return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('error_404.html'), 404
 
 
+@app.route('/api')
+def session_api():
+	return jsonify(list(session.keys()))
